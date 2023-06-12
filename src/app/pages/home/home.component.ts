aa28956ba1e2e7aeca5bf4485d30f53c2f1ebc6d
assim fayas <asim1fayas@gmail.com>
2023-06-12 21:25:50 +0530

    api integrated

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) { }
+
+  bannerResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+
+  //banner data
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult#');
+      this.bannerResult = result.results;
+    });
+  }
+}
+
+
+
+
+
+
---
+++ src/app/service/movie-api-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import{Observable} from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieApiServiceService {
+
+  constructor(private http:HttpClient) { }
+
+baseurl="https://api.themoviedb.org/3"
+apikey="7cc38a599b4f987cb16f7382c8f22b8e"
+
+
+//bannerapidata
+
+bannerApiData():Observable<any>{
+  return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apikey}`);
+}
+
+
+}
